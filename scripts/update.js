bd316ce0c110a7007413bc75f604549a733d674a
Heydon Pickering <heydon@heydonworks.com>
2017-08-21 18:33:53 +0100

    update command

---
+++ scripts/update.js
@@ -0,0 +1,20 @@
+var exec = require('child_process').exec;
+var path = require('path');
+
+var themeFolder = path.join(path.resolve('.'), 'themes');
+console.log(themeFolder);
+var oldTheme = path.join(themeFolder, 'infusion');
+
+exec('mv '+oldTheme+' '+oldTheme+'-old-version-'+Date.now(), function(error, stdout, stderr) {
+  if (error !== null) {
+    console.error('Error saving old infusion version.');
+    return;
+  }
+  exec ('svn export https://github.com/ThePacielloGroup/infusion/trunk/themes/infusion '+oldTheme, function(error, stdout, stderr) {
+    if (error !== null) {
+      console.error('SVN error exporting infusion from Github. Is SVN installed?');
+      return;
+    }
+    console.log('Infusion updated to latest version!');
+  });
+});
