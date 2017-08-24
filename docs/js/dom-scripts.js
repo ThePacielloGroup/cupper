f0f2c8b7cea9eaeeb4e801b9af64a3721d80f73f
Heydon Pickering <heydon@heydonworks.com>
2017-08-24 17:01:28 +0100

    template element polyfill

--- docs/js/dom-scripts.js
+++ docs/js/dom-scripts.js
@@ -130,3 +130,30 @@
     }
   });
 }());
+
+/* Template element polyfill */
+(function templatePolyfill(d) {
+	if('content' in d.createElement('template')) {
+		return false;
+	}
+
+	var qPlates = d.getElementsByTagName('template'),
+		plateLen = qPlates.length,
+		elPlate,
+		qContent,
+		contentLen,
+		docContent;
+
+	for(var x=0; x<plateLen; ++x) {
+		elPlate = qPlates[x];
+		qContent = elPlate.childNodes;
+		contentLen = qContent.length;
+		docContent = d.createDocumentFragment();
+
+		while(qContent[0]) {
+			docContent.appendChild(qContent[0]);
+		}
+
+		elPlate.content = docContent;
+	}
+})(document);
